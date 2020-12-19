2aa4aebf4e3ac85cb6a53ab67932fed00ead2ee9
StroboPC <stefanocalzolari98@gmail.com>
2020-12-19 16:29:41 +0100

    start

---
+++ Assets/MouseLook.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public Transform playerTransform;
+    private float cameraTilt = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; //blocca il cursore al centro dello schermo
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        //Debug.Log("x = " + mouseX);
+        //Debug.Log("y = " + mouseY);
+
+        cameraTilt -= mouseY; //aggiorno la variabile di rotazione della camera tilting
+        cameraTilt = Mathf.Clamp(cameraTilt, -90f, 90f);
+
+        playerTransform.Rotate(Vector3.up * mouseX); //ruoto il player attorno all'asse y (alto)
+        transform.localRotation = Quaternion.Euler(cameraTilt, 0, 0);
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 10;
+    public float jumpHeight = 2;
+    private float gravity = -9.81f;
+    private Vector3 velocity = new Vector3(0,0,0);
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    private bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position,groundDistance,groundMask);
+
+        float x = Input.GetAxis("Horizontal"); //asse a e d (in unity asse x)
+        float z = Input.GetAxis("Vertical"); // asse w ed s (in unity asse z)
+
+        Vector3 move = transform.right.normalized * x + transform.forward.normalized * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (isGrounded && velocity.y < 0) velocity.y = -2f;
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            velocity.y = Mathf.Sqrt(2f * -gravity * jumpHeight); 
+            Debug.Log("velocity.y =" + velocity.y);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
