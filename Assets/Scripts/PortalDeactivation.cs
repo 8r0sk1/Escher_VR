fd20c8aafe1a06ce27b12ba059b4d71b9c4d61ca
StroboPC <stefanocalzolari98@gmail.com>
2021-02-16 18:08:59 +0100

    - SFROGO

---
+++ Assets/Scripts/PortalDeactivation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalDeactivation : MonoBehaviour
+{
+    public GameObject portal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter()
+    {
+        portal.SetActive(false);
+    }
+}
