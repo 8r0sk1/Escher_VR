d6a558dc5d362c3e83057194a3dac375df427c89
DESKTOP-3MCEBPM\Asus <giorgio.arrigo@hotmail.it>
2021-02-26 11:34:50 +0100

    Audioguide script

---
+++ Assets/Scripts/Audioguide.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audioguide : MonoBehaviour
+{
+    public LayerMask audioguideMask;
+    public Camera playerCamera;
+    public float triggerDistance;
+    AudioSource audioguide = null;
+
+    void Start()
+    {
+        audioguide = this.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, triggerDistance, audioguideMask))
+            {
+                audioguide.Play();
+            }
+        }
+    }
+}
